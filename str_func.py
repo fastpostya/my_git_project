8f4f6a86c1a9213c4caff37cdc894ac65258e44b
fastpostya <fastpost@rambler.ru>
2023-01-21 21:47:27 +0300

    add doc string str_capitalize

--- str_func.py
+++ str_func.py
@@ -1,10 +1,16 @@
-def str_func(text)->str:
+def str_func(text) -> str:
     """
-    Function return text with every letter in word 
+    Function return text with every letter in word capital
     :param text: str
     :return: str
     """
     return text.upper()
 
-def str_capitalize(text)->str:
+
+def str_capitalize(text) -> str:
+    """
+    Function return text with first letter in word capital
+    :param text: str
+    :return: str
+    """
     return text.capitalize()
